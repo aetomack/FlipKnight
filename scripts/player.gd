dd95f88cfea4c36824e14e75ac2291d693e76a4c
Alexander E Tomack <alexander.e.tomack@vanderbilt.edu>
2025-01-18 14:15:14 -0600

    moving platform

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,6 +3,10 @@ extends CharacterBody2D
 
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
+const DOUBLETAP_DELAY = 0.25
+var doubletap_time = DOUBLETAP_DELAY
+var last_key = 0
+var CAN_JUMP = false
 
 
 func _physics_process(delta: float) -> void:
@@ -11,8 +15,14 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor() and !CAN_JUMP:
 		velocity.y = JUMP_VELOCITY
+		CAN_JUMP = true
+		
+	# Double jump.
+	if Input.is_action_just_pressed("ui_accept") and !is_on_floor() and CAN_JUMP:
+		velocity.y = JUMP_VELOCITY
+		CAN_JUMP = false
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
